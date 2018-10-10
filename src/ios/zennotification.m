332785b4666e26f9483130d8ce2ebd33b24ba42e
WebGarh Solutions Private Limited <webgarhsolutions@WebGarhs-iMac-2.local>
2018-10-10 11:10:06 +0530

    first commit

---
+++ src/ios/zennotification.m
@@ -0,0 +1,28 @@
+/********* zennotification.m Cordova Plugin Implementation *******/
+
+#import <Cordova/CDV.h>
+
+@interface zennotification : CDVPlugin {
+  // Member variables go here.
+}
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command;
+@end
+
+@implementation zennotification
+
+- (void)coolMethod:(CDVInvokedUrlCommand*)command
+{
+    CDVPluginResult* pluginResult = nil;
+    NSString* echo = [command.arguments objectAtIndex:0];
+
+    if (echo != nil && [echo length] > 0) {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:echo];
+    } else {
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR];
+    }
+
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+@end
